af7a520624e66b0893191f3ff0bd732e91e65ef0
sanguohistory <155078056+sanguohistory@users.noreply.github.com>
2024-02-28 22:54:20 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '77ff59e4b9c34658bf9b02821f059192',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: '问题',
+  domain: 'https://deadpan-bulb-529.notion.site/77ff59e4b9c34658bf9b02821f059192?pvs=4',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
